b18e5ccbe8d9074f86aeabca174e75e41fe0ba25
emiste <emiste@gmail.com>
2013-07-14 04:44:36 +0000

    fix issues

--- web.js
+++ web.js
@@ -7,7 +7,7 @@ app.get('/', function(request, response) {
  var buffer = new Buffer(1024);
  fs.readFileSync('./index.html',function (err, buffer) {
   if (err) throw err;
-  response.send(buffer.toString('utf-8'));   
+  response.send(buffer.toString());   
 }); 
 });
 
