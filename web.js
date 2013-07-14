6700c9c1bb0c0b8649e4dfd1cba03872603e8e9d
emiste <emiste@gmail.com>
2013-07-14 04:40:00 +0000

    render index.html

--- web.js
+++ web.js
@@ -3,7 +3,11 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+ var buffer = new Buffer(1024);
+ fs.readFileSync('./index.html',function (err, buffer) {
+  if (err) throw err;
+  response.send(buffer.toString('utf-8'));   
+}); 
 });
 
 var port = process.env.PORT || 5000;
